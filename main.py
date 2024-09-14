c5991042354432ae0b1b87cb0f99c0a0db87372c
bkandh30 <kandhari.bhavya20@gmail.com>
2024-09-13 18:24:31 -0700

    added support for top 250 movies

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-
-#/opt/homebrew/bin/chromedriver
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
 
 url = "https://www.imdb.com/chart/top/"
-headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.content, 'html.parser')
+
+driver.get(url)
+
+time.sleep(5)
+
+webpage = driver.page_source
+
+soup = BeautifulSoup(webpage, 'html.parser')
+
+driver.quit()
+
 movies = soup.find_all('li', class_='ipc-metadata-list-summary-item')
 data = []
 
@@ -22,7 +31,7 @@ for movie in movies:
     watched = ""
     data.append([title, year, rating, watched])
 
-print(data)
 print(len(data))
 df = pd.DataFrame(data, columns=['Title', 'Year', 'Rating', "Watched"])
 df.to_excel('imdb_top250.xlsx')
+print("Created the excel file containing the top 250 imdb movies")
